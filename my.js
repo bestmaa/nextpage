d4a18633f559ae676256a3417791f07513b6b2d3
Aditya <bestemplin@gmail.com>
2022-06-28 13:38:46 +0530

    add url

--- my.js
+++ my.js
@@ -7,11 +7,13 @@ function getMobileOperatingSystem() {
     }
 
     if (/android/i.test(userAgent)) {
-       alert("this device Android");
+        window.location="https://play.google.com/store/apps/details?id=com.nexusmalls.elantenexusone"
+    //    alert("this device Android");
     }
 
     if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-        alert("this device iOS");
+        window.location="https://apps.apple.com/us/app/nexusone/id1600539826"
+        // alert("this device iOS");
     }
 
     // return "unknown";
